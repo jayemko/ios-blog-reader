088daf5d5f905af321b3abf50b2b0ca88fdcbd4d
Jason Koceja <jason@koceja.com>
2013-08-26 15:46:39 -0700

    Initial Commit

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Jason Koceja on 8/26/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by Jason Koceja on 8/26/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
