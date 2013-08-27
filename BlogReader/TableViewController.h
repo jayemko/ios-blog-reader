200a3d07f6bada4ad99865ed5292819df1c8da82
Jason Koceja <jason@koceja.com>
2013-08-27 16:44:15 -0700

    added a table view controller class

---
+++ BlogReader/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  BlogReader
+//
+//  Created by Jason Koceja on 8/26/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *titles;
+
+@end
